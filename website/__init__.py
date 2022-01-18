96e97f39a9a5fc97931d8db020909a2b2fb77839
Aakarsh-Kumar <aakarsh2504@gmail.com>
2022-01-18 22:19:17 +0530

    DEP

--- website/__init__.py
+++ website/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 db = SQLAlchemy()
 
-ENV = 'DEV'
+ENV = 'DEP'
 DB_NAME = 'database.db'
 URL = 'postgresql://uyfdoacixjhlgx:97f99901a2e54cea748bb32960523cdf589912e58b213f4660058c4b9a5644ef@ec2-54-196-105-177.compute-1.amazonaws.com:5432/d8p6moob0r03gl'
 # URL = 'postgresql://nwkgocqjrnbdnl:079b698c90f7ad4a6562fd41bfcb500720e9b6486c02626f2175a02f2f4f3e33@ec2-34-199-200-115.compute-1.amazonaws.com:5432/d109afai1n644g'
